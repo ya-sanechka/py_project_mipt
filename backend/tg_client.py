d3bf713f31d0f8c28bdd127931010d6c280e3cd0
sasha <08alexspir@gmail.com>
2026-04-21 11:59:17 +0300

    get groups

--- backend/tg_client.py
+++ backend/tg_client.py
@@ -9,7 +9,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION_NAME = "new_session"
 
-proxy_tuple = ('168.222.254.174', 443, '968cc5da3ebacdc3f0c99ba9e2d82d07')
+proxy_tuple = ('mtproxy.neverspy.online', 443, 'dde8653d2faf392a302d829d79537abbe7')
 
 client = TelegramClient(
     SESSION_NAME,
@@ -24,5 +24,25 @@ async def authorize():
     user = await client.get_me()
     print(f"Successfully {user.first_name} (@{user.username})")
 
+
+async def get_groups():
+    groups = []
+    async for dialog in client.iter_dialogs():
+        if dialog.is_group or (dialog.is_channel and dialog.entity.megagroup):
+            groups.append({
+                'id': dialog.id,
+                'name': dialog.name
+            })
+    return groups
+
+async def main():
+    await authorize()
+    groups = await get_groups()
+    print("\nСписок групп:")
+    i = 1
+    for g in groups:
+        print(f"{1}. ID: {g['id']} название: {g['name']}")
+        i += 1
+
 if __name__ == "__main__":
-    asyncio.run(authorize())
+    asyncio.run(main())
