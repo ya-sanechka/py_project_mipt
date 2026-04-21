0b97f06070b5dd7da9eed4bddb091ecea197a6e2
sasha <08alexspir@gmail.com>
2026-04-21 16:09:23 +0300

    get messages from chat

--- backend/tg_client.py
+++ backend/tg_client.py
@@ -20,29 +20,6 @@ client = TelegramClient(
 )
 
 async def authorize():
-    await client.start()
-    user = await client.get_me()
-    print(f"Successfully {user.first_name} (@{user.username})")
-
-
-async def get_groups():
-    groups = []
-    async for dialog in client.iter_dialogs():
-        if dialog.is_group or (dialog.is_channel and dialog.entity.megagroup):
-            groups.append({
-                'id': dialog.id,
-                'name': dialog.name
-            })
-    return groups
-
-async def main():
-    await authorize()
-    groups = await get_groups()
-    print("\nСписок групп:")
-    i = 1
-    for g in groups:
-        print(f"{i}. ID: {g['id']} название: {g['name']}")
-        i += 1
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    if not client.is_connected():
+        await client.start()
+    return await client.get_me()
---
+++ backend/tg_fetch_message.py
@@ -0,0 +1,80 @@
+import json
+
+from tg_client import client, authorize
+users_cache = {}
+async def get_groups():
+    await authorize()
+    groups = []
+    async for dialog in client.iter_dialogs():
+        if dialog.is_group or (dialog.is_channel and dialog.entity.megagroup):
+            groups.append({
+                'id': dialog.id,
+                'name': dialog.name
+            })
+    user = await client.get_me()
+    save_json(groups, f"{user.username}_groups.json")
+    return groups
+
+async def get_chat_users(chat_id):
+    await authorize()
+    chat_users = {}
+    chat = await client.get_entity(chat_id)
+    async for user in client.iter_participants(chat):
+        first_name = user.first_name if user.first_name else ''
+        second_name = user.last_name if user.last_name else ''
+        user_name = user.username
+        chat_users[user.id] = {
+            'username': f"@{user_name}",
+            'full_name': first_name + second_name
+        }
+    user = await client.get_me()
+    save_json(chat_users, f"{user.username}_chat_{chat.title}_users.json")
+    return chat_users
+async def fetch_messages(chat_id, limit=1000):
+    chat_users = await get_chat_users(chat_id)
+    await authorize()
+    messages = []
+    chat = await client.get_entity(chat_id)
+    async for m in client.iter_messages(chat, limit=limit):
+        if m:
+            mentioned = []
+            if m.entities:
+                for el in m.entities:
+                    if hasattr(el, 'user_id'):
+                        mentioned.append(el.user_id)
+            reactions = []
+            if m.reactions:
+                for el in m.reactions.results:
+                    if hasattr(el.reaction, 'emoticon'):
+                        emoji = el.reaction.emoticon
+                    else:
+                        emoji = f"premium_emoji_{el.reaction.document_id}"
+                    r_dict = {
+                        'type_icon': emoji,
+                        'count': el.count
+                    }
+                    reactions.append(r_dict)
+
+            m_dict = {
+                'm_id': m.id,
+                'date': m.date.isoformat(),
+                'sender_id': m.sender_id,
+                'sender_username': chat_users[m.sender_id]['username'],
+                'sender_full_name': chat_users[m.sender_id]['full_name'],
+                'text': m.text,
+                'reply_m_id': m.reply_to_msg_id,
+                'mentioned_users': mentioned,
+                'reactions': reactions
+                #мб еще полей добавить
+
+            }
+            messages.append(m_dict)
+    user = await client.get_me()
+    save_json(messages, f"{user.username}_messages_chat{chat.title}.json")
+    return messages
+
+#async def get_reaction_senders() можно
+
+def save_json(data, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
